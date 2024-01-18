5551284bd3ce106f12263e96c70105c2f9cd3130
Nicolas Casatti <ncasatti@gmail.com>
2024-01-18 15:01:01 -0300

    Setup prisma schema and configuration

---
+++ src/utils/prisma.js
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+  log: [
+    { level: 'warn', emit: 'event' },
+    { level: 'info', emit: 'event' },
+    { level: 'error', emit: 'event' },
+  ],
+  errorFormat: 'minimal',
+})
+
+prisma.$on('warn', (e) => {
+  console.log(e)
+})
+
+prisma.$on('info', (e) => {
+  console.log(e)
+})
+
+prisma.$on('error', (e) => {
+  console.log(e)
+})
+
+export default prisma
